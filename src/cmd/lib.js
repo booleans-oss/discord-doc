904cf232c88d58e755d118e7ac492de48eb50619
booleans-oss <40752807+booleans-oss@users.noreply.github.com>
2020-08-25 16:38:52 +0400

    🌐create libCommand
    Creating lib command which fetches common error and cheatsheets inside the file

---
+++ src/cmd/lib.js
@@ -0,0 +1,24 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+const mistakes = require('../../assets/mistakes.json');
+module.exports = class LibCommand extends BaseCommand {
+  constructor() {
+    super('lib', 'lib', []);
+  }
+
+  async run(client, message, args) {
+      if(args[0] === "cs") {
+        mistakes['djs'].forEach(object => {
+            if (object.alias.includes(args[0].toLowerCase())) return message.channel.send({
+              embed: object.embed_structure
+            })
+          })
+      }
+      if(args[0] === "err") {
+        mistakes['errors'].forEach(object => {
+            if (object.alias.includes(args.join(" ").toLowerCase())) return message.channel.send({
+              embed: object.embed_structure
+            })
+          })
+      }
+  }
+}
