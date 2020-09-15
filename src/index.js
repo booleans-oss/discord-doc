fe77e0f62256ef19824e102ee385f2845a0b0f55
booleans-oss <40752807+booleans-oss@users.noreply.github.com>
2020-09-15 11:52:13 +0200

    🚧 refactoring index

--- src/index.js
+++ src/index.js
@@ -1,35 +1,9 @@
-const Discord = require("discord.js");
-const fs = require("fs");
-const client = new Discord.Client();
+require('dotenv').config()
+const Bot = require("./util/BotBase");
+const bot = new Bot({ partials: ['MESSAGE', 'REACTION']});
 
-require("./util/eventHandler")(client)
-client.commands = new Discord.Collection();
-
-client.login("token");
-
-fs.readdir("./cmd/", (err, files) => {
-    if(err) console.log(err);
-    let jsfile = files.filter(f => f.split(".").pop() === "js")
-    if(jsfile.length <= 0) return
-
-    jsfile.forEach((f, i) => {
-        let props = require(`./cmd/${f}`);
-        client.commands.set(props.help.name, props)
-    })
-})
-client.on("message", async message => {
-    if(message.author.bot) return;
-    if(message.channel.type === 'dm') return;
-
-    if(!message.content.startsWith("!")) return;
-    let messageArray = message.content.split(" ");
-    let command = messageArray[0];
-    let args = messageArray.slice(1);
-
-    let commandFile = client.commands.get(command.slice(1));
-    if(commandFile) {
-        message.delete();
-        commandFile.run(client, message, args)
-    }
-
-});
+(async () => {
+    await bot.login(process.env.TOKEN_BOT);
+    await bot.chargementCommand('../../cmd/');
+    await bot.chargementEvent(bot);
+})();
