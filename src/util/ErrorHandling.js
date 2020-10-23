e318227afda19e704f8044b38053a16f0b7ea83a
booleans-oss <40752807+booleans-oss@users.noreply.github.com>
2020-10-23 13:27:06 +0200

    🚧 (Client) Error Handling Improvements

--- src/util/ErrorHandling.js
+++ src/util/ErrorHandling.js
@@ -1,7 +1,8 @@
-const fs = require('fs')
+const fs = require('fs');
+const { execSync } = require('child_process')
 const verif = async function () {
     let mandatory_modules = ["discord.js", "Dotenv", "discord.js-docs", "axios", "moment", "turndown"];
-    await Promise.all(mandatory_modules.map(module => { if(!isModuleInstalled(module)) throw new Error(`\x1b[33m%s\x1b[0m`, `${module} n'a pas été installé. --- `, `\x1b[0m`, `npm i ${module}`) }))
+    await Promise.all(mandatory_modules.map(module => { isModuleInstalled(module)}))
 }
 
 function isModuleInstalled(name) {
@@ -9,9 +10,13 @@ function isModuleInstalled(name) {
         require.resolve(name);
         return true;
     } catch(e){
-
+        
     }
-    return false;
+    console.log(`\x1b[33m%s\x1b[0m`, `${module} n'a pas été installé. --- `, `\x1b[0m`, `npm i ${module}`)
+    console.log(`Installation de ${name} en cours`)
+    execSync(`npm i ${name}`, {
+        cwd: `./`
+      });
 }
 
 module.exports = verif;
